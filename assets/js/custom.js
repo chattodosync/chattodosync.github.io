c0723ba49b9afdc226b1d1d584ec4517dbb3ffe4
nirbhay.texting <nirbhay@textingjoy.com>
2024-08-05 00:59:20 -0400

    coming soon first cut

---
+++ assets/js/custom.js
@@ -0,0 +1,157 @@
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// jQuery
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+$(document).ready(function($) {
+    "use strict";
+
+    $("[data-background-color]").each(function() {
+        $(this).css("background-color", $(this).attr("data-background-color") );
+    });
+
+    $(".bg-transfer").each(function() {
+        $(this).css("background-image", "url("+ $(this).find("img").attr("src") +")" );
+    });
+
+    //  Magnific Popup
+
+    if ($(".image-popup").length > 0) {
+        $(".image-popup").magnificPopup({
+            type: "image",
+            removalDelay: 300,
+            mainClass: "mfp-fade",
+            overflowY: "hidden"
+        });
+    }
+
+    if ($(".video-popup").length > 0) {
+        $(".video-popup").magnificPopup({
+            type: "iframe",
+            removalDelay: 300,
+            mainClass: "mfp-fade",
+            overflowY: "hidden",
+            iframe: {
+                markup: '<div class="mfp-iframe-scaler">'+
+                    '<div class="mfp-close"></div>'+
+                    '<iframe class="mfp-iframe" frameborder="0" allowfullscreen></iframe>'+
+                    '</div>',
+                patterns: {
+                    youtube: {
+                        index: 'youtube.com/',
+                        id: 'v=',
+                        src: '//www.youtube.com/embed/%id%?autoplay=1'
+                    },
+                    vimeo: {
+                        index: 'vimeo.com/',
+                        id: '/',
+                        src: '//player.vimeo.com/video/%id%?autoplay=1'
+                    },
+                    gmaps: {
+                        index: '//maps.google.',
+                        src: '%id%&output=embed'
+                    }
+                },
+                srcAction: 'iframe_src'
+            }
+        });
+    }
+
+    // Element fade in animation
+
+    $(".animate").each(function(e) {
+        var $this = $(this);
+        setTimeout(function(){
+            $this.addClass("idle");
+        }, e * 100);
+    });
+
+    //  Count Down
+
+    if( $(".count-down").length ){
+        var year = parseInt( $(".count-down").attr("data-countdown-year"), 10 );
+        var month = parseInt( $(".count-down").attr("data-countdown-month"), 10 ) - 1;
+        var day = parseInt( $(".count-down").attr("data-countdown-day"), 10 );
+        $(".count-down").countdown({until: new Date(year, month, day), padZeroes: true});
+    }
+
+});
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// On Load
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+$(window).load(function(){
+    $(".animate").addClass("in");
+});
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// Functions
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+function simpleMap(latitude, longitude, markerImage, mapTheme, mapElement){
+
+    if ( mapTheme == "light" ){
+        var mapStyles = [{"featureType":"water","elementType":"geometry.fill","stylers":[{"color":"#d3d3d3"}]},{"featureType":"transit","stylers":[{"color":"#808080"},{"visibility":"off"}]},{"featureType":"road.highway","elementType":"geometry.stroke","stylers":[{"visibility":"on"},{"color":"#b3b3b3"}]},{"featureType":"road.highway","elementType":"geometry.fill","stylers":[{"color":"#ffffff"}]},{"featureType":"road.local","elementType":"geometry.fill","stylers":[{"visibility":"on"},{"color":"#ffffff"},{"weight":1.8}]},{"featureType":"road.local","elementType":"geometry.stroke","stylers":[{"color":"#d7d7d7"}]},{"featureType":"poi","elementType":"geometry.fill","stylers":[{"visibility":"on"},{"color":"#ebebeb"}]},{"featureType":"administrative","elementType":"geometry","stylers":[{"color":"#a7a7a7"}]},{"featureType":"road.arterial","elementType":"geometry.fill","stylers":[{"color":"#ffffff"}]},{"featureType":"road.arterial","elementType":"geometry.fill","stylers":[{"color":"#ffffff"}]},{"featureType":"landscape","elementType":"geometry.fill","stylers":[{"visibility":"on"},{"color":"#efefef"}]},{"featureType":"road","elementType":"labels.text.fill","stylers":[{"color":"#696969"}]},{"featureType":"administrative","elementType":"labels.text.fill","stylers":[{"visibility":"on"},{"color":"#737373"}]},{"featureType":"poi","elementType":"labels.icon","stylers":[{"visibility":"off"}]},{"featureType":"poi","elementType":"labels","stylers":[{"visibility":"off"}]},{"featureType":"road.arterial","elementType":"geometry.stroke","stylers":[{"color":"#d6d6d6"}]},{"featureType":"road","elementType":"labels.icon","stylers":[{"visibility":"off"}]},{},{"featureType":"poi","elementType":"geometry.fill","stylers":[{"color":"#dadada"}]}];
+    }
+    else if ( mapTheme == "dark" ){
+        mapStyles = [{"featureType":"all","elementType":"labels.text.fill","stylers":[{"saturation":36},{"color":"#000000"},{"lightness":40}]},{"featureType":"all","elementType":"labels.text.stroke","stylers":[{"visibility":"on"},{"color":"#000000"},{"lightness":16}]},{"featureType":"all","elementType":"labels.icon","stylers":[{"visibility":"off"}]},{"featureType":"administrative","elementType":"geometry.fill","stylers":[{"color":"#000000"},{"lightness":20}]},{"featureType":"administrative","elementType":"geometry.stroke","stylers":[{"color":"#000000"},{"lightness":17},{"weight":1.2}]},{"featureType":"landscape","elementType":"geometry","stylers":[{"color":"#000000"},{"lightness":20}]},{"featureType":"poi","elementType":"geometry","stylers":[{"color":"#000000"},{"lightness":21}]},{"featureType":"road.highway","elementType":"geometry.fill","stylers":[{"color":"#000000"},{"lightness":17}]},{"featureType":"road.highway","elementType":"geometry.stroke","stylers":[{"color":"#000000"},{"lightness":29},{"weight":0.2}]},{"featureType":"road.arterial","elementType":"geometry","stylers":[{"color":"#000000"},{"lightness":18}]},{"featureType":"road.local","elementType":"geometry","stylers":[{"color":"#000000"},{"lightness":16}]},{"featureType":"transit","elementType":"geometry","stylers":[{"color":"#000000"},{"lightness":19}]},{"featureType":"water","elementType":"geometry","stylers":[{"color":"#000000"},{"lightness":17}]}]
+    }
+    var mapCenter = new google.maps.LatLng(latitude,longitude);
+    var mapOptions = {
+        zoom: 13,
+        center: mapCenter,
+        disableDefaultUI: true,
+        scrollwheel: false,
+        styles: mapStyles
+    };
+    var element = document.getElementById(mapElement);
+    var map = new google.maps.Map(element, mapOptions);
+    var marker = new google.maps.Marker({
+        position: new google.maps.LatLng(latitude,longitude),
+        map: map,
+        icon: markerImage
+    });
+}
+
+
+document.addEventListener('DOMContentLoaded',function(event){
+  // array with texts to type in typewriter
+  var dataText = [ "✉️✉️✉️✉️✉️✉️✉️✉️✉️✉️✉️", "Make Your Messages Meaningful", "Take Action on Important Conversations", "Collaboratively Share Your Work", "Feel Accomplished Every Day ✅"];
+
+  // type one text in the typwriter
+  // keeps calling itself until the text is finished
+  function typeWriter(text, i, fnCallback) {
+    // chekc if text isn't finished yet
+    if (i < (text.length)) {
+      // add next character to h1
+     document.querySelector(".subtext").innerHTML = text.substring(0, i+1) +'<span aria-hidden="true"></span>';
+
+      // wait for a while and call this function again for next character
+      setTimeout(function() {
+        typeWriter(text, i + 1, fnCallback)
+      }, 50);
+    }
+    // text finished, call callback if there is a callback function
+    else if (typeof fnCallback == 'function') {
+      // call callback after timeout
+      setTimeout(fnCallback, 2200);
+    }
+  }
+  // start a typewriter animation for a text in the dataText array
+   function StartTextAnimation(i) {
+     if (typeof dataText[i] == 'undefined'){
+        setTimeout(function() {
+          StartTextAnimation(1);
+        }, 2000);
+     }
+     // check if dataText[i] exists
+     else if (i < dataText[i].length) {
+      // text exists! start typewriter animation
+     typeWriter(dataText[i], 0, function(){
+       // after callback (and whole text has been animated), start next text
+       StartTextAnimation(i + 1);
+     });
+    }
+  }
+
+  StartTextAnimation(0);
+});
