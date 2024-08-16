874f8a0ee349a98f98b813da07a85b0948d39f2b
amEya <ameyakulkarni2023@gmail.com>
2024-08-17 01:40:02 +0530

    added new firebaseconfig

--- assets/js/mail.js
+++ assets/js/mail.js
@@ -1,13 +1,24 @@
+// const firebaseConfig = {
+//     apiKey: "AIzaSyC8ud5SaUnU5rFBDm0-2nA42W4R7-F14xk",
+//     authDomain: "chattodo-pre-launch.firebaseapp.com",
+//     databaseURL: "https://chattodo-pre-launch-default-rtdb.firebaseio.com",
+//     projectId: "chattodo-pre-launch",
+//     storageBucket: "chattodo-pre-launch.appspot.com",
+//     messagingSenderId: "438482623621",
+//     appId: "1:438482623621:web:5da7062199346fda045dec",
+//     measurementId: "G-LQRV6BKBG8"
+// };
+
 const firebaseConfig = {
-    apiKey: "AIzaSyC8ud5SaUnU5rFBDm0-2nA42W4R7-F14xk",
-    authDomain: "chattodo-pre-launch.firebaseapp.com",
-    databaseURL: "https://chattodo-pre-launch-default-rtdb.firebaseio.com",
-    projectId: "chattodo-pre-launch",
-    storageBucket: "chattodo-pre-launch.appspot.com",
-    messagingSenderId: "438482623621",
-    appId: "1:438482623621:web:5da7062199346fda045dec",
-    measurementId: "G-LQRV6BKBG8"
-};
+    apiKey: "AIzaSyA-hCL2fNdF3RD_0APX8eVnPTUQNe25C_0",
+    authDomain: "chattodo-apps.firebaseapp.com",
+    databaseURL: "https://chattodo-apps-default-rtdb.firebaseio.com",
+    projectId: "chattodo-apps",
+    storageBucket: "chattodo-apps.appspot.com",
+    messagingSenderId: "1035673617495",
+    appId: "1:1035673617495:web:96f2fe8ee126aa0ada4f12",
+    measurementId: "G-24CF4X1N99"
+  };
 
 firebase.initializeApp(firebaseConfig);
 const analytics = firebase.analytics();
